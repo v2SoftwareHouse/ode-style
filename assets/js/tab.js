cd2bf7a89adf73f35f8e5e6814d2be04a0b46f80
v2SoftwareHouse <v2.softwarehouse@gmail.com>
2024-05-22 16:11:04 -0300

    review tabs

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -1,13 +1,13 @@
   // Show the first tab by default
-  $('.tabs-stage div').hide();
-  $('.tabs-stage div:first').show();
-  $('.tabs-nav li:first').addClass('tab-active');
+  $('.tabs-stage-code div').hide();
+  $('.tabs-stage-code div:first').show();
+  $('.tabs-nav-code li:first').addClass('tab-active-code');
 
   // Change tab class and display content
   $('.tabs-nav a').on('click', function(event){
     event.preventDefault();
-    $('.tabs-nav li').removeClass('tab-active');
-    $(this).parent().addClass('tab-active');
-    $('.tabs-stage div').hide();
+    $('.tabs-nav-code li').removeClass('tab-active-code');
+    $(this).parent().addClass('tab-active-code');
+    $('.tabs-stage-code div').hide();
     $($(this).attr('href')).show();
   });
