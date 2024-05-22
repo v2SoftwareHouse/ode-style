a42cbf0b3dcb6298487d9320d05d7213afcd8c12
v2SoftwareHouse <v2.softwarehouse@gmail.com>
2024-05-22 20:38:05 -0300

    review code tabs

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -1,13 +1,13 @@
-  // Show the first tab by default
-  $('.tabs-stage-code div').hide();
-  $('.tabs-stage-code div:first').show();
-  $('.tabs-nav-code li:first').addClass('tab-active-code');
-
-  // Change tab class and display content
-  $('.tabs-nav a').on('click', function(event){
-    event.preventDefault();
-    $('.tabs-nav-code li').removeClass('tab-active-code');
-    $(this).parent().addClass('tab-active-code');
-    $('.tabs-stage-code div').hide();
-    $($(this).attr('href')).show();
-  });
+function openPage(evt, cityName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.firstElementChild.className += " w3-border-red";
+}
