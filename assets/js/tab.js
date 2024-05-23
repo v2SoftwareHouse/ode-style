4070d62e70d2555d5f8680d91c0ea5519409eb5b
v2SoftwareHouse <v2.softwarehouse@gmail.com>
2024-05-22 21:41:54 -0300

    up tabs css

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -1,13 +1,8 @@
-function openPage(evt, cityName) {
-    var i, x, tablinks;
-    x = document.getElementsByClassName("city");
+function openPage(cityName) {
+    var i;
+    var x = document.getElementsByClassName("city");
     for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
+       x[i].style.display = "none";  
     }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" w3-border-blue", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.firstElementChild.className += " w3-border-blue";
+    document.getElementById(cityName).style.display = "block";  
 }
