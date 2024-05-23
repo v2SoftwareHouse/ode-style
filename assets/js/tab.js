8ab8c492212298752a1a1cd5896cc75619542c38
v2SoftwareHouse <v2.softwarehouse@gmail.com>
2024-05-22 21:09:45 -0300

    up tabs css

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -6,8 +6,8 @@ function openPage(evt, cityName) {
     }
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
+      tablinks[i].className = tablinks[i].className.replace(" w3-border-blue", "");
     }
     document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.firstElementChild.className += " w3-border-red";
+    evt.currentTarget.firstElementChild.className += " w3-border-blue";
 }
